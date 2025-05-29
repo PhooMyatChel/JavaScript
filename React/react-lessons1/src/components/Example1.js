168182cb9e6b6ae1244e7b6dcfa12651c793ac86
PhooMyatChel <phoomyatchel8834@gmail.com>
2025-05-29 01:30:47 -0700

    react-js-tutorial

---
+++ React/react-lessons1/src/components/Example1.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+export default class Person extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>I am Class Component123</h1>
+        </div>
+
+        );
+    }
+}
---
+++ React/react-lessons1/src/components/Example2.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+export default function Person(){
+        return(
+        <div>
+            <h1>I am Functional Component.</h1>
+        </div>
+
+        );
+    }
+
