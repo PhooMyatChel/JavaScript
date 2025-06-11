f686b5a2950d5da90ec46fd30669234a43d0bb4c
PhooMyatChel <phoomyatchel8834@gmail.com>
2025-06-11 01:39:54 -0700

    react routing,drawer

--- React/nextjs-mui/src/app/layout.js
+++ React/nextjs-mui/src/app/layout.js
@@ -1,11 +1,15 @@
 import{AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
 import StyledRoot from "./StyledRoot";
+import DrawerNav from "@/components/DrawerNav";
+
 export default function RootLayout ({children}){
   return(
     <html lang="en">
       <body>
         <AppRouterCacheProvider>
-          <StyledRoot> {children} </StyledRoot> 
+          <StyledRoot> 
+            <DrawerNav />
+            {children} </StyledRoot> 
         </AppRouterCacheProvider>
       </body>
     </html>
